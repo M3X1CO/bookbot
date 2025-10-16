8f9c47f62b4d2f8e68471905884627aa8343ddae
Howard Felix <Howard.felix@outlook.com>
2025-10-16 11:45:17 +0700

    count

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ def get_book_text(filepath):
     with open(filepath, "r", encoding="utf-8") as f:
         return f.read()
 
-
+def get_word_count(verse):
+    word_count = verse.split()
+    return len(word_count)
 
 
 def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
-    print(text)
+    count = get_word_count(text)
 
-
+    print(f'Found {count} total words')
 
 main()
