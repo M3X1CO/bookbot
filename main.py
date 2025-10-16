3e55e5265b5d68f32dacc7dc6b2ab49586d26992
Howard Felix <Howard.felix@outlook.com>
2025-10-16 13:30:06 +0700

    addict

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_word_count, get_letter_count
 
 def get_book_text(filepath):
     with open(filepath, "r", encoding="utf-8") as f:
@@ -7,8 +7,11 @@ def get_book_text(filepath):
 def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
+    
     count = get_word_count(text)
-
     print(f'Found {count} total words')
+    
+    char_count = get_letter_count(text)
+    print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_word_count(verse):
     word_count = verse.split()
     return len(word_count)
+
+def get_letter_count(verse):
+    # create a dic to hold letter 'a': 324 and count
+    char_count = {}
+    for char in verse.lower():
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
